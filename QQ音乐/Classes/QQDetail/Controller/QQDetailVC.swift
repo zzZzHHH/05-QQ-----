cdd8287e8ba33d6f05996ec497a7fab5e6b8f5e1
xmg <xmg@appledeimac.local>
2016-06-04 14:35:57 +0800

    基本结构搭建

---
+++ QQ音乐/Classes/QQDetail/Controller/QQDetailVC.swift
@@ -0,0 +1,35 @@
+//
+//  QQDetailVC.swift
+//  QQ音乐
+//
+//  Created by xmg on 16/6/4.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+import UIKit
+
+class QQDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
